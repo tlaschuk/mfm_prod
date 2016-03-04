e03d46ec27eaf8948bbece8a8c4b5596746ca130
Terry Laschuk <tlaschuk@sympatico.ca>
2016-03-04 08:01:07 -0500

    Updated link.

--- locales/en/license.js
+++ locales/en/license.js
@@ -7,7 +7,7 @@ var license =
         "list"  :
             [
             "Install the <a href=\"http://www3.sympatico.ca/tlaschuk/mapleflagmissions/MFM_License.exe\">license program</a> and determine your System Unique ID Key (SUID). Consult our license program installation <a href=\"https://drive.google.com/file/d/0B4GJ-2wKWbw_ZUxlNUVpWjdKcHc/view?usp=sharing\">guide</a> if you require guidance.",
-            "Make your purchase for any of the missions or campaign packages from the <a href=\"/mission\">mission page</a>. Only if this is your first purchase, send us an email at <a href=\"mailto:mapleflagmissions@gmail.com\">mapleflagmissions@gmail.com</a> listing your name, email address, and the SUID Key. We will send you the Mission Key to your email account once the transaction has been processed.",
+            "Make your purchase for any of the missions or campaign packages from the <a href=\"http://tlaschuk.github.io/missions\">mission page</a>. Only if this is your first purchase, send us an email at <a href=\"mailto:mapleflagmissions@gmail.com\">mapleflagmissions@gmail.com</a> listing your name, email address, and the SUID Key. We will send you the Mission Key to your email account once the transaction has been processed.",
             "Use the provided Mission Key to unlock and install the missions. Consult our mission installation <a href=\"https://drive.google.com/file/d/0B4GJ-2wKWbw_b3JfcGdKREZNRkE/view?usp=sharing\">guide</a> if you require guidance."
             ]
         }
